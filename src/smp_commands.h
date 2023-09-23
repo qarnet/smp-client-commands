ecdd52d1e0a1beabb410eb52336a1e7856eecb26
qarnet <thomaskirschner85@yahoo.de>
2023-09-23 23:36:02 +0200

    first commit

---
+++ src/smp_commands.h
@@ -0,0 +1,30 @@
+#ifndef _SMP_COMMANDS_H_
+#define _SMP_COMMANDS_H_
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <errno.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+#include <zcbor_encode.h>
+#include <zcbor_decode.h>
+#include <zcbor_common.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <bluetooth/gatt_dm.h>
+#include <zephyr/sys/byteorder.h>
+#include <bluetooth/scan.h>
+#include <bluetooth/services/dfu_smp.h>
+#include <dk_buttons_and_leds.h>
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/flash.h>
+
+#include <pm_config.h>
+
+#endif
