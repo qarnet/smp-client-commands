015e54e0dd4ca98da11f682facf7cc93c745e2d3
qarnet <thomaskirschner85@yahoo.de>
2023-10-10 20:17:40 +0200

    current non working general response state

--- src/mgmt/default_os_mgmt.c
+++ src/mgmt/default_os_mgmt.c
@@ -8,13 +8,16 @@
 #define SMP_ECHO_MAP_VALUE_MAX_LEN 30
 static struct smp_buffer smp_rsp_buff;
 
+struct k_sem smp_sem;
+K_SEM_DEFINE(smp_sem, 1, 1);
+
 static void smp_rsp_proc(struct bt_dfu_smp *dfu_smp)
 {
 	uint8_t *p_outdata = (uint8_t *)(&smp_rsp_buff);
 	const struct bt_dfu_smp_rsp_state *rsp_state;
 
 	rsp_state = bt_dfu_smp_rsp_state(dfu_smp);
-	printk("Echo response part received, size: %zu.\n",
+	printk("response part received, size: %zu.\n",
 	       rsp_state->chunk_size);
 
 	if (rsp_state->offset + rsp_state->chunk_size > sizeof(smp_rsp_buff)) {
@@ -51,30 +54,50 @@ static void smp_rsp_proc(struct bt_dfu_smp *dfu_smp)
 		default: printk("Unexpected command group (%u)!\n", group); return;
 	}
 
-
 	if(smp_rsp_buff.header.id == SMP_CID_OS_CONSOLE)
 	{
+	size_t payload_len = ((uint16_t)smp_rsp_buff.header.len_h8) << 8 |
+				      smp_rsp_buff.header.len_l8;
 
+		zcbor_state_t zsd[CBOR_DECODER_STATE_NUM];
+		struct zcbor_string value = {0};
+		bool ok;
+
+		zcbor_new_decode_state(zsd, ARRAY_SIZE(zsd), smp_rsp_buff.payload, payload_len, 1);
+
+		/* Stop decoding on the error. */
+		zsd->constant_state->stop_on_error = true;
+
+		zcbor_map_start_decode(zsd);
+		
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_key[value.len];
+		memcpy(map_key, value.value, value.len);
+
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_value[value.len];
+		memcpy(map_value, value.value, value.len);
+
+		zcbor_map_end_decode(zsd);
+
+		if (zcbor_check_error(zsd)) {
+			/* Print textual representation of the received CBOR map. */
+			printk("{_\"%s\": \"%s\"}\n", map_key, map_value);
+		} else {
+			printk("Cannot print received CBOR stream (err: %d)\n",
+			       zcbor_pop_error(zsd));
+		}
 	}
 	else if(smp_rsp_buff.header.id == SMP_CID_OS_ECHO)
 	{
-		printk("Total response received - decoding\n");
-		if (smp_rsp_buff.header.op != SMP_OID_WRITE_RESPONSE) {
-			printk("Unexpected operation code (%u)!\n",
-			       smp_rsp_buff.header.op);
-			return;
-		}
-		uint16_t group = ((uint16_t)smp_rsp_buff.header.group_h8) << 8 |
-				      smp_rsp_buff.header.group_l8;
-		if (group != SMP_GID_OS /* OS */) {
-			printk("Unexpected command group (%u)!\n", group);
-			return;
-		}
-		if (smp_rsp_buff.header.id != SMP_CID_OS_ECHO /* ECHO */) {
-			printk("Unexpected command (%u)",
-			       smp_rsp_buff.header.id);
-			return;
-		}
 		size_t payload_len = ((uint16_t)smp_rsp_buff.header.len_h8) << 8 |
 				      smp_rsp_buff.header.len_l8;
 
@@ -117,70 +140,11 @@ static void smp_rsp_proc(struct bt_dfu_smp *dfu_smp)
 	}
 	else if(smp_rsp_buff.header.id == SMP_CID_OS_INFO)
 	{
-		
-	}
-	else if(smp_rsp_buff.header.id == SMP_CID_OS_MCUMGR_PARAMS)
-	{
-		
-	}
-	else if(smp_rsp_buff.header.id == SMP_CID_OS_MEM_POOL_STAT)
-	{
-		
-	}
-	else if(smp_rsp_buff.header.id == SMP_CID_OS_RESET)
-	{
-		
-	}
-	else if(smp_rsp_buff.header.id == SMP_CID_OS_TASK_STAT)
-	{
-		
-	}
-
-}
-
-static void smp_echo_rsp_proc(struct bt_dfu_smp *dfu_smp)
-{
-	uint8_t *p_outdata = (uint8_t *)(&smp_rsp_buff);
-	const struct bt_dfu_smp_rsp_state *rsp_state;
-
-	rsp_state = bt_dfu_smp_rsp_state(dfu_smp);
-	printk("Echo response part received, size: %zu.\n",
-	       rsp_state->chunk_size);
-
-	if (rsp_state->offset + rsp_state->chunk_size > sizeof(smp_rsp_buff)) {
-		printk("Response size buffer overflow\n");
-	} else {
-		p_outdata += rsp_state->offset;
-		memcpy(p_outdata,
-		       rsp_state->data,
-		       rsp_state->chunk_size);
-	}
-
-	if (bt_dfu_smp_rsp_total_check(dfu_smp)) {
-		printk("Total response received - decoding\n");
-		if (smp_rsp_buff.header.op != SMP_OID_WRITE_RESPONSE) {
-			printk("Unexpected operation code (%u)!\n",
-			       smp_rsp_buff.header.op);
-			return;
-		}
-		uint16_t group = ((uint16_t)smp_rsp_buff.header.group_h8) << 8 |
-				      smp_rsp_buff.header.group_l8;
-		if (group != SMP_GID_OS /* OS */) {
-			printk("Unexpected command group (%u)!\n", group);
-			return;
-		}
-		if (smp_rsp_buff.header.id != SMP_CID_OS_ECHO /* ECHO */) {
-			printk("Unexpected command (%u)",
-			       smp_rsp_buff.header.id);
-			return;
-		}
 		size_t payload_len = ((uint16_t)smp_rsp_buff.header.len_h8) << 8 |
 				      smp_rsp_buff.header.len_l8;
 
 		zcbor_state_t zsd[CBOR_DECODER_STATE_NUM];
 		struct zcbor_string value = {0};
-		char map_key[SMP_ECHO_MAP_KEY_MAX_LEN];
-		char map_value[SMP_ECHO_MAP_VALUE_MAX_LEN];
 		bool ok;
 
 		zcbor_new_decode_state(zsd, ARRAY_SIZE(zsd), smp_rsp_buff.payload, payload_len, 1);
@@ -190,39 +154,21 @@ static void smp_echo_rsp_proc(struct bt_dfu_smp *dfu_smp)
 
 		zcbor_map_start_decode(zsd);
 		
-		ok = zcbor_tstr_decode(zsd, &value);
-
-		if (!ok) {
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
 			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
 			return;
-		} else if ((value.len != 1) || (*value.value != 'r')) {
-			printk("Invalid data received.\n");
-			return;
-		} else {
-			/* Do nothing */
 		}
+		char map_key[value.len];
+		memcpy(map_key, value.value, value.len);
 
-		map_key[0] = value.value[0];
-
-		/* Add string NULL terminator */
-		map_key[1] = '\0';
-
-		ok = zcbor_tstr_decode(zsd, &value);
-
-		if (!ok) {
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
 			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
 			return;
-		} else if (value.len > (sizeof(map_value) - 1)) {
-			printk("To small buffer for received data.\n");
-			return;
-		} else {
-			/* Do nothing */
 		}
-
+		char map_value[value.len];
 		memcpy(map_value, value.value, value.len);
-
-		/* Add string NULL terminator */
-		map_value[value.len] = '\0';
 
 		zcbor_map_end_decode(zsd);
 
@@ -234,6 +180,195 @@ static void smp_echo_rsp_proc(struct bt_dfu_smp *dfu_smp)
 			       zcbor_pop_error(zsd));
 		}
 	}
+	else if(smp_rsp_buff.header.id == SMP_CID_OS_MCUMGR_PARAMS)
+	{
+		size_t payload_len = ((uint16_t)smp_rsp_buff.header.len_h8) << 8 |
+				      smp_rsp_buff.header.len_l8;
+
+		zcbor_state_t zsd[CBOR_DECODER_STATE_NUM];
+		struct zcbor_string value = {0};
+		bool ok;
+
+		zcbor_new_decode_state(zsd, ARRAY_SIZE(zsd), smp_rsp_buff.payload, payload_len, 2);
+
+		/* Stop decoding on the error. */
+		zsd->constant_state->stop_on_error = true;
+
+		zcbor_map_start_decode(zsd);
+		
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_key[value.len];
+		memcpy(map_key, value.value, value.len);
+
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_value[value.len];
+		memcpy(map_value, value.value, value.len);
+
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_key_2[value.len];
+		memcpy(map_key_2, value.value, value.len);
+
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_value_2[value.len];
+		memcpy(map_value_2, value.value, value.len);
+
+		zcbor_map_end_decode(zsd);
+
+		if (zcbor_check_error(zsd)) {
+			/* Print textual representation of the received CBOR map. */
+			printk("{_\"%s\": \"%s\"}\n", map_key_2, map_value_2);
+		} else {
+			printk("Cannot print received CBOR stream (err: %d)\n",
+			       zcbor_pop_error(zsd));
+		}
+	}
+	else if(smp_rsp_buff.header.id == SMP_CID_OS_MEM_POOL_STAT)
+	{
+		size_t payload_len = ((uint16_t)smp_rsp_buff.header.len_h8) << 8 |
+				      smp_rsp_buff.header.len_l8;
+
+		zcbor_state_t zsd[CBOR_DECODER_STATE_NUM];
+		struct zcbor_string value = {0};
+		bool ok;
+
+		zcbor_new_decode_state(zsd, ARRAY_SIZE(zsd), smp_rsp_buff.payload, payload_len, 1);
+
+		/* Stop decoding on the error. */
+		zsd->constant_state->stop_on_error = true;
+
+		zcbor_map_start_decode(zsd);
+		
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_key[value.len];
+		memcpy(map_key, value.value, value.len);
+
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_value[value.len];
+		memcpy(map_value, value.value, value.len);
+
+		zcbor_map_end_decode(zsd);
+
+		if (zcbor_check_error(zsd)) {
+			/* Print textual representation of the received CBOR map. */
+			printk("{_\"%s\": \"%s\"}\n", map_key, map_value);
+		} else {
+			printk("Cannot print received CBOR stream (err: %d)\n",
+			       zcbor_pop_error(zsd));
+		}
+	}
+	else if(smp_rsp_buff.header.id == SMP_CID_OS_RESET)
+	{
+		 size_t payload_len = ((uint16_t)smp_rsp_buff.header.len_h8) << 8 |
+				      smp_rsp_buff.header.len_l8;
+
+		zcbor_state_t zsd[CBOR_DECODER_STATE_NUM];
+		struct zcbor_string value = {0};
+		bool ok;
+
+		zcbor_new_decode_state(zsd, ARRAY_SIZE(zsd), smp_rsp_buff.payload, payload_len, 1);
+
+		/* Stop decoding on the error. */
+		zsd->constant_state->stop_on_error = true;
+
+		zcbor_map_start_decode(zsd);
+		
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_key[value.len];
+		memcpy(map_key, value.value, value.len);
+
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_value[value.len];
+		memcpy(map_value, value.value, value.len);
+
+		zcbor_map_end_decode(zsd);
+
+		if (zcbor_check_error(zsd)) {
+			/* Print textual representation of the received CBOR map. */
+			printk("{_\"%s\": \"%s\"}\n", map_key, map_value);
+		} else {
+			printk("Cannot print received CBOR stream (err: %d)\n",
+			       zcbor_pop_error(zsd));
+		}
+	}
+	else if(smp_rsp_buff.header.id == SMP_CID_OS_TASK_STAT)
+	{
+		size_t payload_len = ((uint16_t)smp_rsp_buff.header.len_h8) << 8 |
+				      smp_rsp_buff.header.len_l8;
+
+		zcbor_state_t zsd[CBOR_DECODER_STATE_NUM];
+		struct zcbor_string value = {0};
+		bool ok;
+
+		zcbor_new_decode_state(zsd, ARRAY_SIZE(zsd), smp_rsp_buff.payload, payload_len, 1);
+
+		/* Stop decoding on the error. */
+		zsd->constant_state->stop_on_error = true;
+
+		zcbor_map_start_decode(zsd);
+		
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_key[value.len];
+		memcpy(map_key, value.value, value.len);
+
+		if(!zcbor_tstr_decode(zsd, &value))
+		{
+			printk("Decoding error (err: %d)\n", zcbor_pop_error(zsd));
+			return;
+		}
+		char map_value[value.len];
+		memcpy(map_value, value.value, value.len);
+
+		zcbor_map_end_decode(zsd);
+
+		if (zcbor_check_error(zsd)) {
+			/* Print textual representation of the received CBOR map. */
+			printk("{_\"%s\": \"%s\"}\n", map_key, map_value);
+		} else {
+			printk("Cannot print received CBOR stream (err: %d)\n",
+			       zcbor_pop_error(zsd));
+		}
+	}
+	else
+	{
+	}
+
+	k_sem_give(&smp_sem);
 }
 
 int smp_echo(struct bt_dfu_smp *dfu_smp)
@@ -241,6 +376,8 @@ int smp_echo(struct bt_dfu_smp *dfu_smp)
 	static struct smp_buffer smp_cmd;
 	zcbor_state_t zse[CBOR_ENCODER_STATE_NUM];
 	size_t payload_len;
+
+	k_sem_take(&smp_sem, K_SECONDS(1));
 
 	zcbor_new_encode_state(zse, ARRAY_SIZE(zse), smp_cmd.payload,
 			       sizeof(smp_cmd.payload), 0);
@@ -269,7 +406,7 @@ int smp_echo(struct bt_dfu_smp *dfu_smp)
 	smp_cmd.header.seq = 0;
 	smp_cmd.header.id  = SMP_CID_OS_ECHO;
 
-	return bt_dfu_smp_command(dfu_smp, smp_echo_rsp_proc,
+	return bt_dfu_smp_command(dfu_smp, smp_rsp_proc,
 				  sizeof(smp_cmd.header) + payload_len,
 				  &smp_cmd);
 }
@@ -279,6 +416,8 @@ int smp_task_stats(struct bt_dfu_smp *dfu_smp)
 	static struct smp_buffer smp_cmd;
 	zcbor_state_t zse[CBOR_ENCODER_STATE_NUM];
 	size_t payload_len;
+
+	k_sem_take(&smp_sem, K_SECONDS(1));
 
 	zcbor_new_encode_state(zse, ARRAY_SIZE(zse), smp_cmd.payload,
 			       sizeof(smp_cmd.payload), 0);
@@ -305,7 +444,7 @@ int smp_task_stats(struct bt_dfu_smp *dfu_smp)
 	smp_cmd.header.seq = 0;
 	smp_cmd.header.id  = SMP_CID_OS_TASK_STAT;
 
-	return bt_dfu_smp_command(dfu_smp, smp_echo_rsp_proc,
+	return bt_dfu_smp_command(dfu_smp, smp_rsp_proc,
 				  sizeof(smp_cmd.header) + payload_len,
 				  &smp_cmd);
 }
@@ -315,6 +454,8 @@ int smp_mem_pool_stats(struct bt_dfu_smp *dfu_smp)
 	static struct smp_buffer smp_cmd;
 	zcbor_state_t zse[CBOR_ENCODER_STATE_NUM];
 	size_t payload_len;
+
+	k_sem_take(&smp_sem, K_SECONDS(1));
 
 	zcbor_new_encode_state(zse, ARRAY_SIZE(zse), smp_cmd.payload,
 			       sizeof(smp_cmd.payload), 0);
@@ -341,7 +482,7 @@ int smp_mem_pool_stats(struct bt_dfu_smp *dfu_smp)
 	smp_cmd.header.seq = 0;
 	smp_cmd.header.id  = SMP_CID_OS_MEM_POOL_STAT;
 
-	return bt_dfu_smp_command(dfu_smp, smp_echo_rsp_proc,
+	return bt_dfu_smp_command(dfu_smp, smp_rsp_proc,
 				  sizeof(smp_cmd.header) + payload_len,
 				  &smp_cmd);
 }
@@ -351,6 +492,8 @@ int smp_sys_reset(struct bt_dfu_smp *dfu_smp)
 	static struct smp_buffer smp_cmd;
 	zcbor_state_t zse[CBOR_ENCODER_STATE_NUM];
 	size_t payload_len;
+
+	k_sem_take(&smp_sem, K_SECONDS(1));
 
 	zcbor_new_encode_state(zse, ARRAY_SIZE(zse), smp_cmd.payload,
 			       sizeof(smp_cmd.payload), 0);
@@ -377,7 +520,7 @@ int smp_sys_reset(struct bt_dfu_smp *dfu_smp)
 	smp_cmd.header.seq = 0;
 	smp_cmd.header.id  = SMP_CID_OS_RESET;
 
-	return bt_dfu_smp_command(dfu_smp, smp_echo_rsp_proc,
+	return bt_dfu_smp_command(dfu_smp, smp_rsp_proc,
 				  sizeof(smp_cmd.header) + payload_len,
 				  &smp_cmd);
 }
@@ -387,6 +530,8 @@ int smp_mcumgr_params(struct bt_dfu_smp *dfu_smp)
 	static struct smp_buffer smp_cmd;
 	zcbor_state_t zse[CBOR_ENCODER_STATE_NUM];
 	size_t payload_len;
+
+	k_sem_take(&smp_sem, K_SECONDS(1));
 
 	zcbor_new_encode_state(zse, ARRAY_SIZE(zse), smp_cmd.payload,
 			       sizeof(smp_cmd.payload), 0);
@@ -413,7 +558,7 @@ int smp_mcumgr_params(struct bt_dfu_smp *dfu_smp)
 	smp_cmd.header.seq = 0;
 	smp_cmd.header.id  = SMP_CID_OS_MCUMGR_PARAMS;
 
-	return bt_dfu_smp_command(dfu_smp, smp_echo_rsp_proc,
+	return bt_dfu_smp_command(dfu_smp, smp_rsp_proc,
 				  sizeof(smp_cmd.header) + payload_len,
 				  &smp_cmd);
 }
@@ -423,6 +568,8 @@ int smp_bootloader_info(struct bt_dfu_smp *dfu_smp)
 	static struct smp_buffer smp_cmd;
 	zcbor_state_t zse[CBOR_ENCODER_STATE_NUM];
 	size_t payload_len;
+
+	k_sem_take(&smp_sem, K_SECONDS(1));
 
 	zcbor_new_encode_state(zse, ARRAY_SIZE(zse), smp_cmd.payload,
 			       sizeof(smp_cmd.payload), 0);
@@ -451,7 +598,7 @@ int smp_bootloader_info(struct bt_dfu_smp *dfu_smp)
 	smp_cmd.header.seq = 0;
 	smp_cmd.header.id  = SMP_CID_OS_INFO;
 
-	return bt_dfu_smp_command(dfu_smp, smp_echo_rsp_proc,
+	return bt_dfu_smp_command(dfu_smp, smp_rsp_proc,
 				  sizeof(smp_cmd.header) + payload_len,
 				  &smp_cmd);
 }
